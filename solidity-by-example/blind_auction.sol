e56a77a6a09de0279fc93a4060643ccef3e91cea
emmaodia <odia.emma@gmail.com>
2022-02-26 23:24:16 +0100

    blind auction contract.

--- solidity-by-example/blind_auction.sol
+++ solidity-by-example/blind_auction.sol
@@ -1,79 +1,133 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.9 <0.9.0
 
-contract OpenAuction {
+pragma solidity >=0.70 <0.9.0;
+
+contract BlindAuction {
+    struct Bid {
+        bytes32 blindedBid;
+        uint deposit;
+    }
+
     address payable public beneficiary;
-    uint public auctionEndTime;
+
+    uint public biddingEnd;
+    uint public revealEnd;
+    bool public ended;
+
+    mapping(address => Bid[]) public bids;
 
     address public highestBidder;
     uint public highestBid;
 
     mapping(address => uint) pendingReturns;
 
-    bool ended;
+    event AuctionEnded(address winner, uint highestBid);
 
-    event HighestBidIncreased(address bidder, uint amount);
-    event AuctionEnded(address winner, uint amount);
+    event WithDrawSuccessful(address _to, uint _value);
 
-    error AuctionAlreadyEnded();
-    error BidNotHighEnough(uint highestBid);
-
-    error AuctionNotYetEnded();
+    error TooEarly(uint time);
+    error TooLate(uint time);
     error AuctionEndAlreadyCalled();
 
-    constructor(
-        uint biddingTime;
-        address payable beneficiaryAddress;
+    modifier onlyBefore(uint time){
+        if(block.timestamp >= time) {
+            revert TooLate(time);
+        }
+        _;
+    }
+
+    modifier onlyAfter(uint time){
+        if(block.timestamp <= time){
+            revert TooEarly(time);
+        }
+        _;
+    }
+
+    constructor (
+        uint _biddingTime,
+        uint _revealTime,
+        address payable _beneficiary
     ){
-        beneficiary = beneficiaryAddress;
-        auctionEndTime = block.timestamp + biddingTime;
+        beneficiary = _beneficiary;
+        biddingEnd = block.timestamp + _biddingTime;
+        revealEnd = biddingEnd + _revealTime;
     }
 
-    function bid() external payable{
-
-        if(block.timestamp > auctionEndTime){
-            revert AuctionAlreadyEnded();
-        }
-
-        if(msg.value <= highestBid){
-            revert BidNotHighEnough(highestBid);
-        }
-
-        if(highestBid != 0){
-            pendingReturns[highestBidder] += highestBid;
-        }
-
-        highestBidder = msg.sender;
-        highestBid = msg.value;
-        emit HighestBidIncreased(msg.sender, msg.value);
+    function bid(bytes32 _blindedBid) external payable onlyBefore(biddingEnd) {
+        bids[msg.sender].push(Bid({
+            blindedBid: _blindedBid,
+            deposit: msg.value
+        }));
     }
 
-    function withdraw() external returns(bool) {
+    function reveal(
+        uint[] calldata values,
+        bool[] calldata fakes,
+        bytes32[] calldata secrets
+        ) external onlyAfter(biddingEnd) onlyBefore(revealEnd) {
+            uint length = bids[msg.sender].length;
+            require(values.length == length);
+            require(fakes.length == length);
+            require(secrets.length == length);
+
+            uint refund;
+            for (uint i; i < length; i++){
+                Bid storage bidToCheck = bids[msg.sender][i];
+                (uint value, bool fake, bytes32 secret) = (values[i], fakes[i], secrets[i]);
+
+                if(bidToCheck.blindedBid != keccak256(abi.encodePacked(value, fake, secret))){
+                    continue;
+                }
+
+                refund += bidToCheck.deposit;
+                if(!fake && bidToCheck.deposit >= value){
+                    if(placeBid(msg.sender, value)){
+                        refund -= value;
+                    }
+                }
+
+                bidToCheck.blindedBid = bytes32(0);
+            }
+
+            payable(msg.sender).transfer(refund);
+    } 
+
+    function withdraw() external {
         uint amount = pendingReturns[msg.sender];
         if(amount > 0){
             pendingReturns[msg.sender] = 0;
 
-            if(!payable(msg.sender).send(amount)) {
-                pendingReturns[msg.sender] = amount;
-                return false;
-            }
+            payable(msg.sender).transfer(amount);
         }
+
+        emit WithDrawSuccessful(msg.sender, amount);
+    }
+
+    function auctionEnd() external onlyAfter(revealEnd) {
+
+        if (ended){
+            revert AuctionEndAlreadyCalled();
+        }
+
+        emit AuctionEnded(highestBidder, highestBid);
+        ended = true;
+
+        beneficiary.transfer(highestBid);
+    }
+
+    function placeBid(address _bidder, uint _value) internal returns(bool success) {
+        if(_value <= highestBid){
+            return false;
+        }
+
+        if(highestBidder != address(0)){
+            pendingReturns[highestBidder] += highestBid;
+        }
+
+        highestBid = _value;
+        highestBidder = _bidder;
 
         return true;
     }
 
-    function auctionEnd() external {
-        if(block.timestamp < auctionEndTime){
-            revert AuctionNotYetEnded();
-        }
-
-        if(ended){
-            revert AuctionAlreadyEnded();
-        }
-
-        ended = true;
-        emit AuctionEnded(highestBidder, highestBid);
-
-        beneficiary.transfer(highestBid);
-    }
-}
+} 
