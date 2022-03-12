172a69e40ff7cb103ed92c4051b35fe158a16b97
emmaodia <odia.emma@gmail.com>
2022-03-12 11:12:44 +0100

    solidity data types

---
+++ solidity-by-example/data_types.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.4;
+
+contract Data_Types {
+
+// The is a state variable address with name deployer. It is set to public and can be called from within the contract.
+address public deployer;
+}
